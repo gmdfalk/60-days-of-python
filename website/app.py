59093d6458055f2f868900d4d1fa8dc8d5f71551
max demian <mikar@gmx.de>
2014-03-24 13:05:47 +0100

    moved from main.py to app.py

--- website/app.py
+++ website/app.py
@@ -2,12 +2,11 @@ from google.appengine.ext import webapp
 
 
 class MainPage(webapp.RequestHandler):
-    
-    
+
+
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.out.write('Hello world!')
 
 
 app = webapp.WSGIApplication([('/', MainPage)], debug=True)
-
