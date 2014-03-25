f41a73c6eba28452b8519a7de2b5ab54de22ad03
max demian <mikar@gmx.de>
2014-03-25 16:56:17 +0100

    working on auth. tomorrow addressbook

--- oop/basics_1/auth/auth.py
+++ oop/basics_1/auth/auth.py
@@ -4,21 +4,111 @@
     @author: Max Demian
 """
 
+import hashlib
+
+class AuthException(Exception):
+
+    def __init__(self, username, user=None):
+        super(AuthException, self).__init__(username, user)
+        self.username = username
+        self.user = user
+
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+
+class PasswordTooShort(AuthException):
+    pass
+
+class InvalidUsername(AuthException):
+    pass
+
+class InvalidPassword(AuthException):
+    pass
+
+class PermissionError(Exception):
+    pass
+
 
 class User(object):
+    "User data structure"
 
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        self.name = username
+        self.password = self._encrypt_pw(password)
+        self.is_logged_in = False
+
+    def _encrypt_pw(self, password):
+        """Encrypt the password with the username and return
+        the sha digest"""
+        hash_str = (self.name + password)
+        hash_str = hash_str.encode("utf8")
+        return hashlib.sha256(hash_str).hexdigest()
+
+    def check_password(self, password):
+        """Return True if the password is valid for this
+        user, otherwise false"""
+        encrypted = self._encrypt_pw(password)
+        return encrypted == self.password
+
+
+class Authenticator(object):
+    "Handles authentication and user management"
+    def __init__(self):
+        "Construct an authenticator to manage users logging in and out."
+        self.users = {}
+
+    def add_user(self, username, password):
+        if username in self.users:
+            raise UsernameAlreadyExists(username)
+        if len(password) < 6:
+            raise PasswordTooShort(username)
+        self.users[username] = User(username, password)
+
+    def login(self, username, password):
+        try:
+            user = self.users[username]
+        except KeyError:
+            raise InvalidUsername(username)
+
+        if not user.check_password(password):
+            raise InvalidPassword(username, user)
+
+        user.is_logged_in = True
+        return True
+
+    def is_logged_in(self, username):
+        if username in self.users:
+            return self.users[username].is_logged_in
+        return False
 
 
 class Authorizor(object):
-    pass
+    "Handles permissions"
+    def __init__(self, authenticator):
+        self.authenticator = authenticator
+        self.permissions = {}
+
+    def add_permission(self, perm_name):
+        "Create a new permission that users can be added to."
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            self.permissions[perm_name] = set()
+        else:
+            raise PermissionError("Permission Exists")
+
+    def permit_user(self, perm_name, username):
+        "Grant the given permission to the user"
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username not in self.authenticator.users:
+                raise InvalidUsername(username)
+            perm_set.add(username)
 
 
-class Manager(object):
-    pass
-
-
-
-
+# authenticator = Authenticator()
--- oop/basics_1/auth/auth_test.py
+++ oop/basics_1/auth/auth_test.py
@@ -5,26 +5,48 @@
 """
 
 import unittest
-from auth import User, Authorizor, Manager
+from auth import User, Authorizor, Authenticator, PasswordTooShort
+from auth import UsernameAlreadyExists, InvalidPassword
 
 
 class AuthTest(unittest.TestCase):
 
 
     def setUp(self):
-        u = User()
-        a = Authorizor()
-        m = Manager()
-        m.add_user()
+        self.authenticator = Authenticator()
+        self.auth = Authorizor(self.authenticator)
+        self.authenticator.add_user("frank", "thetank")
 
-    def test_user(self):
-        u = User("frank", "password")
-        self.assertEqual(u.password, "password")
+    def test_user_exists_in_dictionary(self):
+        self.assertTrue("frank" in self.authenticator.users)
+        self.assertTrue(self.authenticator.users["frank"].check_password("thetank"))
+
+    def test_exception_password_too_short(self):
+        self.assertRaises(PasswordTooShort,
+                          self.authenticator.add_user("tim", "struppi"))
+
+    def test_exception_username_exists(self):
+        with self.assertRaises(UsernameAlreadyExists):
+            self.authenticator.add_user('frank', 'number2')
+
+    def test_exception_invalid_password(self):
+        with self.assertRaises(InvalidPassword):
+            self.authenticator.login("frank", "")
 
     def test_add_user(self):
+        self.authenticator.add_user("tim", "struppi")
+        self.assertTrue("frank" in self.authenticator.users)
+
+    def test_is_logged_in(self):
+        self.assertFalse(self.authenticator.is_logged_in("frank"))
+        self.authenticator.login("frank", "thetank")
+        self.assertTrue(self.authenticator.is_logged_in("frank"))
+
+
+    def test_add_permission(self):
         pass
 
-    def test_authorize(self):
+    def test_permit_user(self):
         pass
 
 if __name__ == "__main__":
