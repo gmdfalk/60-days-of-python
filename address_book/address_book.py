ce471a56024f6877b97aad46a021ad936d4ee15f
max demian <mikar@gmx.de>
2014-03-27 22:28:32 +0100

    database worries

--- address_book/address_book.py
+++ address_book/address_book.py
@@ -1,41 +1,76 @@
 # coding: utf-8
 
-import sqlite3 as lite
+import sqlite3 as sql
+import os
 
-def insert(car_id, car, price):
-    db = lite.connect('addresses.db')
-    with db:
-        cur = db.cursor()
-        cur.execute("INSERT INTO Cars VALUES({}, '{}', {})".format(car_id, car, price))
+class Contacts(object):
+    "Manages your contacts in a sqlite3 database"
 
-# insert(1, "Audi", 5900000)
+    def __init__(self, db_file="addresses.db", table="Addresses"):
+        self.table = table
+        # Create and/or connect to our database.
+        self.db_file = db_file
+        self.db = sql.connect(self.db_file)
+        # Fix encoding issues.
+        self.db.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+        # And create the cursor so we can perform database actions.
+        self.cur = self.db.cursor()
 
-# Bogus list of addresses to work with.
-addresses = (
-    ("Max Demian", 60329, "Frankfurt", "Gutleutstraße 214", "069 741 382", "0176 241 332", "mikar@gmx.net"),
-    ("Kristian Gottlieb", "", "Schellweiler", "Hardenbergerstr. 50", "06381 63 56 46", "212-660-2245", "K.Gottlieb@fake.de"),
-    ("Mathias Hoover", 54340, "Schleich", "Güntzelstr. 12", "06507 58 02 52", "317 461 1232", "hoovercraft@lol.cat"),
-    ("Dirk Vogt", 92303, "Neumarkt", "Luebecker Tordamm 1", "09181 75 77 80", "7024380985", "DirkVogt@armyspy.com"),
-    ("Barbara Baier", 15230, "Frankfurt (Oder)", "Michaelkirchstr. 36", "05704 27 12 54", "1-323-472-0089", "bbaier@jourrapi.de"),
-    ("Jens Kappel", 55481, "Schwarzen", "Joachimstaler Straße 97", "06763 39 39 51", "0700-413 22 5", "tinydancer@gmx.net"),
-)
+    def populate_database(self):
+        "Creates a sample database to work with"
+        addresses = (
+            ("Max Demian", "60329", "Frankfurt", "Gutleutstraße 214",
+             "069 741 382", "0176 241 332", "mikar@gmx.net"),
+            ("Mathias Hoover", "54340", "Schleich", "Güntzelstr. 12",
+             "06507 58 02 52", "317 461 1232", "hoovercraft@lol.cat"),
+            ("Dirk Vogt", "92303", "Neumarkt", "Luebecker Tordamm 1",
+             "09181 75 77 80", "7024380985", "DirkVogt@armyspy.com"),
+            ("Barbara Baier", "15230", "Frankfurt (Oder)", "Kirchstr. 36",
+             "05704 27 12 54", "1-323-472-0089", "bbaier@jourrapi.de")
+        )
 
-# Create and/or connect to our database.
-db = lite.connect('addresses.db')
+        with self.db:
+            self.cur.execute("DROP TABLE IF EXISTS {}".format(self.table))
+            self.cur.execute("CREATE TABLE {}(Name TEXT, ZIP Text, City Text,\
+                              Street Text, Phone Text, Mobile Text,\
+                              Email TEXT)".format(self.table))
+            self.cur.executemany("INSERT INTO {} VALUES(?, ?, ?, ?, ?, ?, ?\
+                                 )".format(self.table), addresses)
 
-# Fix encoding issues.
-db.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+    def insert(self, name="", zip="", city="", street="",
+               phone="", mobile="", email=""):
+        with self.db:
+            self.cur.execute("INSERT INTO {} VALUES({}, {}, {}, {}, {},\
+                              {}, {})".format(self.table, name, zip, city,
+                                             street, phone, mobile, email))
 
-with db:
+    def search(self):
+        pass
 
-    cur = db.cursor()
-    address = ("Max Demian", "63092", "Frankfurt", "Gutleutstraße 214",
-               "069741382", "0176-241-332", "mikar@gmx.net")
+    def show(self):
+        # FIXME: SQLite wants to use ASCII here so we abide, for now.
+        with self.db:
+            self.db.text_factory = str
+            self.cur.execute("SELECT * from {}".format(self.table))
+            for i in self.cur.fetchall():
+                print "{}, {} {}, {}".format(i[0], i[1], i[2], i[3])
+                print "{}, {}, mobile: {}\n".format(i[6], i[4], i[5])
 
-    cur.execute("DROP TABLE IF EXISTS Addresses")
-    cur.execute("CREATE TABLE Addresses(Name TEXT, ZIP Text, City Text, Street Text,\
-                                   Phone Text, Mobile Text, Email TEXT)")
-    cur.execute("INSERT INTO Addresses VALUES(?, ?, ?, ?, ?, ?, ?)", address)
-    cur.execute("""SELECT * FROM Addresses""")
-    next = cur.fetchone()
-    for i in next: print i
+    def delete_entry(self):
+        pass
+
+    def delete_table(self, table=""):
+        if not table:
+            table = self.table
+        with self.db:
+            self.cur.execute("DROP TABLE IF EXISTS {}".format(table))
+
+    def delete_database(self):
+        os.remove(self.db_file)
+
+
+if __name__ == "__main__":
+    c = Contacts()
+    c.populate_database()
+    c.show()
+    c.insert("yes", "hey", "hey", "hey", "hey", "hey", "hey")
