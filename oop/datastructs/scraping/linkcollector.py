d6879d2c5d223d9cb1eb840fba52c32f7c8c9b47
max demian <mikar@gmx.de>
2014-03-29 17:58:03 +0100

    basic web page scraper

---
+++ oop/datastructs/scraping/linkcollector.py
@@ -0,0 +1,57 @@
+"""
+    Created on 29 Mar 2014
+
+    @author: Max Demian
+"""
+
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from urllib.error import HTTPError
+import re
+import sys
+
+
+# FIXME: This regex catches e-mail addresses, too.
+LINK_REGEX = re.compile("<a [^>]*href=['\"]([^'\"]+)['\"][^>]*>")
+
+
+class LinkCollector(object):
+
+    def __init__(self, url):
+        self.url = "http://" + urlparse(url).netloc
+        self.collected_links = {}
+        self.visited_links = set()
+
+    def collect_links(self, path="/"):
+        full_url = self.url + path
+        self.visited_links.add(full_url)
+        page = str(urlopen(full_url).read())
+        links = LINK_REGEX.findall(page)
+        links = {self.normalize_url(path, link) for link in links}
+        self.collected_links[full_url] = links
+        for link in links:
+            self.collected_links.setdefault(link, set())
+        unvisited_links = links.difference(self.visited_links)
+        # 404 will raise a HTTPError so we ignore those.
+        try:
+            for link in unvisited_links:
+                if link.startswith(self.url):
+                    self.collect_links(urlparse(link).path)
+        except HTTPError:
+            pass
+
+    def normalize_url(self, path, link):
+        if link.startswith("http://"):
+            return link
+        elif link.startswith("/"):
+            return self.url + link
+        else:
+            return self.url + path.rpartition("/")[0] + "/" + link
+
+
+
+if __name__ == "__main__":
+    c = LinkCollector("http://localhost:8000")
+    c.collect_links()
+    for link, item in c.collected_links.items():
+        print("{}: {}".format(link, item))
--- oop/datastructs/various.py
+++ oop/datastructs/various.py
@@ -125,3 +125,54 @@ print("bands is to my_artists:")
 print("issuperset: {}".format(bands.issuperset(my_artists)))
 print("issubset: {}".format(bands.issubset(my_artists)))
 print("difference: {}".format(bands.difference(my_artists)))
+
+# Extending the dictionary type to remember the order the keys are added in.
+from collections import KeysView, ItemsView, ValuesView
+class DictSorted(dict):
+
+    def __new__(*args, **kwargs):
+        new_dict = dict.__new__(*args, **kwargs)
+        new_dict.ordered_keys = []
+        return new_dict
+
+    def __setitem__(self, key, value):
+        '''self[key] = value syntax'''
+        if key not in self.ordered_keys:
+            self.ordered_keys.append(key)
+        super(DictSorted, self).__setitem__(key, value)
+
+    def setdefault(self, key, value):
+        if key not in self.ordered_keys:
+            self.ordered_keys.append(key)
+        return super(DictSorted, self).setdefault(key, value)
+
+    def keys(self):
+        return KeysView(self)
+
+    def values(self):
+        return ValuesView(self)
+
+    def items(self):
+        return ItemsView(self)
+
+    def __iter__(self):
+        '''for x in self syntax'''
+        return self.ordered_keys.__iter__()
+
+ds = DictSorted()
+d = {}
+ds["a"] = 1
+ds["b"] = 2
+ds.setdefault("c", 3)
+d["a"] = 1
+d["b"] = 2
+d.setdefault("c", 3)
+
+for k, v in ds.items():
+    print k, v
+
+for k, v in d.items():
+    print k, v
+
+# Note: collections actually has this functionality with OrderedDict (along with
+# other interesting data structures like namedtuple).
