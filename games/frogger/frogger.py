3b3045472bf1651e067b849ef08862a6de4f4d9f
max demian <mikar@gmx.de>
2014-04-01 18:38:47 +0200

    spending way too much time on the start screen

--- games/frogger/frogger.py
+++ games/frogger/frogger.py
@@ -9,36 +9,54 @@ import pygame
 import sys
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def wait_for_input():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: # pressing escape quits
+                    terminate()
+                return
+    
 def start_screen():
     # Load music and loop it until the start screen ends.
     pygame.mixer.music.load("data/theme.mp3")
     pygame.mixer.music.play(-1)
-    
-    black, red = (0, 0, 0), (255, 0, 0)
+
+    # Draw the start screen.
+    blue, white = (0, 0, 71), (255,255,255)
     start_font = pygame.font.Font("data/emulogic.ttf", 20)
-    start_screen = True
-    
-    while start_screen:
-        window.fill(black)
-#         myfont = pygame.font.SysFont("Britannic Bold", 40)
-        nlabel1 = start_font.render("Welcome", 1, red)
-        nlabel2 = start_font.render("to", 1, red)
-        nlabel3 = start_font.render("Frogger", 1, red)
-        window.blit(nlabel1, (150, 200))
-        window.blit(nlabel2, (200, 250))
-        window.blit(nlabel3, (150, 300))
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                start_screen = False
-                pygame.mixer.music.fadeout(2000)
-        pygame.display.flip()
-
-def main():    
+    start_title = pygame.image.load("data/frogger_title.gif")
+    window.fill(blue)
+    nlabel1 = start_font.render("Press any key", 1, white)
+    nlabel2 = start_font.render("to", 1, white)
+    nlabel3 = start_font.render("continue", 1, white)
+    window.blit(nlabel1, (85, 200))
+    window.blit(nlabel2, (195, 250))
+    window.blit(nlabel3, (135, 300))
+    window.blit(start_title, (40,100))
+    pygame.display.flip()
     while True:
+        pass
+    # Press "ANY" key to continue.
+    wait_for_input()
+    pygame.mixer.music.fadeout(2000)
 
-        keys = pygame.key.get_pressed()
-
+def main():
+    while True:
+        # Cap the framerate at 60 FPS.
+        clock = pygame.time.Clock()
+        clock.tick(60)
+        
+        frog = pygame.Rect((0, 0), (32, 32))
+        
         # Map Arrow Keys to movements.
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             frog.centerx -= 4
         if keys[pygame.K_RIGHT]:
@@ -53,18 +71,17 @@ def main():
         window.fill((55, 155, 255), frog)
         pygame.display.update()
 
-        # Cap the framerate at 60 FPS.
-        clock.tick(60)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
 if __name__ == '__main__':
+    # Initialize Pygame and Window to draw in.
     pygame.init()
     window = pygame.display.set_mode((446, 512), 0, 32)
-    clock = pygame.time.Clock()
-    frog = pygame.Rect((0, 0), (32, 32))
-    #~ frog = pygame.image.load("data/frog.png")
+
+    # ~ frog = pygame.image.load("data/frog.png")
     start_screen()
     main()
