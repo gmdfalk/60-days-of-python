7ef86e11ae0027cb19fccad557cd7bb04c2754be
max demian <mikar@gmx.de>
2014-04-02 18:05:54 +0200

    added some sprites, a to-do list and the enemy class

--- games/frogger/frogger.py
+++ games/frogger/frogger.py
@@ -3,10 +3,20 @@
     Created on 1 Apr 2014
 
     @author: Max Demian
+
+    Todo:
+    * Frog always ontop
+    * Drowning
+    * Collision + Lives
+    * Home Zone
+    * Animations (Frog + Turtles)
+    * Sounds & Music
+    * Scores and Highscores
 """
 
 import pygame
 import sys
+from random import choice, randrange, random
 
 
 class Obstacle(object):
@@ -16,40 +26,59 @@ class Obstacle(object):
         self.go_left = direction
         self.x = x
         self.y = y
-        self.startpos = (x, y)
         self.img = pygame.image.load(img)
         self.rect = self.img.get_rect()
 
     def draw(self):
+        "Moves and then draws the obstacle"
         # Adjust the position of the obstacle.
         if self.go_left:
             self.x -= 2
         else:
             self.x += 2
         # Reset the object if it moves out of screen.
-#         if abs(self.x - self.startpos[0]) > 480:
-#             self.x, self.y = self.startpos
-        if self.x > 660:
-            self.x = 0
-        elif self.x < -120:
+        # To accomodate the big logs and introduce gaps, we use -180, not 0.
+        if self.x > 480:
+            self.x = -180
+        elif self.x < -180:
             self.x = 480
         # And finally draw it.
         window.blit(self.img, (self.x, self.y))
+
 
 class Car(Obstacle):
 
     def __init__(self, x, y, img, direction=0):
         super(Car, self).__init__(x, y, img, direction)
 
+
 class Turtle(Obstacle):
 
     def __init__(self, x, y, img, direction=0):
         super(Turtle, self).__init__(x, y, img, direction)
 
+
 class Log(Obstacle):
 
     def __init__(self, x, y, img, direction=0):
         super(Log, self).__init__(x, y, img, direction)
+
+
+class Enemy(object):
+
+    def __init__(self):
+        self.y = 80
+        self.spawns = [420, 320, 220, 120, 20]
+        self.imgs = ["data/croc.png", "data/fly.png"]
+
+    def draw(self):
+        if random() > 0.6:
+            return
+        self.img = pygame.image.load(choice(self.imgs))
+        self.duration = randrange(5, 10)
+
+        window.blit(self.img, (choice(self.spawns), self.y))
+
 
 class Frog(object):
 
@@ -61,6 +90,7 @@ class Frog(object):
         self.status = self.img_f
         self.x = 200
         self.y = 560
+        self.lives = 4
 
     def draw(self):
         self.rect = self.status
@@ -82,31 +112,33 @@ class Frog(object):
         self.status = self.img_b
         self.y += 40
 
+
 def wait_for_input():
+    # Allow these keys to cancel the loop.
+    valid_keys = [pygame.K_ESCAPE, pygame.K_SPACE, pygame.K_RETURN]
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:  # pressing escape quits
-                    terminate()
-                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key in valid_keys:
+                    # Return to wherever we were called from.
+                    return
+
 
 def pause():
     pause_font = pygame.font.Font("data/emulogic.ttf", 20)
     pause_label = pause_font.render("paused", 1, (255, 255, 255))
     window.blit(pause_label, (180, 300))
     pygame.display.flip()
-    print "pause"
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:  # pressing escape quits
-                    return
+    print "paused"
+    wait_for_input()
+
 
 def terminate():
     pygame.quit()
     sys.exit()
+
 
 def start_screen():
     # Load music and loop it until the start screen ends.
@@ -118,12 +150,12 @@ def start_screen():
     start_font = pygame.font.Font("data/emulogic.ttf", 20)
     start_title = pygame.image.load("data/frogger_title.gif")
     window.fill(blue)
-    nlabel1 = start_font.render("Press any key", 1, white)
-    nlabel2 = start_font.render("to", 1, white)
-    nlabel3 = start_font.render("continue", 1, white)
-    window.blit(nlabel1, (110, 300))
-    window.blit(nlabel2, (215, 350))
-    window.blit(nlabel3, (160, 400))
+    label1 = start_font.render("Press Enter", 1, white)
+    label2 = start_font.render("to", 1, white)
+    label3 = start_font.render("continue", 1, white)
+    window.blit(label1, (140, 300))
+    window.blit(label2, (215, 350))
+    window.blit(label3, (160, 400))
     window.blit(start_title, (60, 150))
 
     # Update the screen only once.
@@ -137,27 +169,27 @@ def create_road():
     road = []
     ys = [520, 480, 440, 400, 360]
     x = 0
-    for i in range(2):
+    for _ in range(2):
         car = Car(x, ys[0], "data/car_1.png", 1)
         road.append(car)
         x += 144
     x = 20
-    for i in range(3):
+    for _ in range(3):
         car = Car(x, ys[1], "data/car_2.png")
         road.append(car)
         x += 128
     x = 40
-    for i in range(3):
+    for _ in range(3):
         car = Car(x, ys[2], "data/car_3.png", 1)
         road.append(car)
         x += 128
     x = 60
-    for i in range(2):
+    for _ in range(2):
         car = Car(x, ys[3], "data/car_4.png")
         road.append(car)
         x += 128
     x = 80
-    for i in range(2):
+    for _ in range(2):
         car = Car(x, ys[4], "data/car_5.png", 1)
         road.append(car)
         x += 176
@@ -168,27 +200,27 @@ def create_river():
     river = []
     ys = [120, 160, 200, 240, 280]
     x = 0
-    for i in range(4):
+    for _ in range(4):
         turtle = Turtle(x, ys[4], "data/turtle_3_full.png", 1)
         river.append(turtle)
         x += 128
     x = 20
-    for i in range(3):
+    for _ in range(3):
         log = Log(x, ys[3], "data/tree_small.png")
         river.append(log)
         x += 192
     x = 40
-    for i in range(2):
+    for _ in range(2):
         log = Log(x, ys[2], "data/tree_big.png")
         river.append(log)
         x += 256
     x = 60
-    for i in range(4):
+    for _ in range(4):
         turtle = Turtle(x, ys[1], "data/turtle_2_full.png", 1)
         river.append(turtle)
         x += 112
     x = 80
-    for i in range(3):
+    for _ in range(3):
         log = Log(x, ys[0], "data/tree_medium.png")
         river.append(log)
         x += 176
@@ -197,24 +229,25 @@ def create_river():
 
 def main():
 
-    background = pygame.image.load("data/background.png")
+    # Basic setup.
     clock = pygame.time.Clock()
+    background = pygame.image.load("data/background.png")
     f = Frog()
-    road = create_road()
-    river = create_river()
-    objects = [f] + road + river
+    e = Enemy()
+    road, river = create_road(), create_river()
+    # Merge all movable objects into one list to allow easy iteration.
+    objects = [f] + road + river  # + [e]
     level = 0
 
     while True:
-        # Draw the images.
+        # Draw all our objects and images before we refresh the screen.
         window.blit(background, (0, 0))
         for i in objects:
             i.draw()
 
-        # Flip the buffer every 30 ms.
         pygame.display.flip()
-        # Since we're lazy, we just increase the Clock per level, thus
-        # speeding up the whole game.
+        # For now, we just increase the FPS per level, effectively speeding
+        # up the whole game.
         clock.tick(30 + (level * 10))
 
         # Main event loop.
@@ -239,10 +272,9 @@ def main():
 
 
 if __name__ == '__main__':
-    # Initialize Pygame, the screen/window and our globals..
+    # Initialize Pygame, the screen/window and some globals.
     pygame.init()
     window = pygame.display.set_mode((480, 600), 0, 32)
     game_zone = window.get_rect()
-    # ~ frog = pygame.image.load("data/frog.png")
     start_screen()
     main()
