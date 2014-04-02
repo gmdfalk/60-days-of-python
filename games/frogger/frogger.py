43d7038339fb41f54ad13d265c059b0c98c1720c
max demian <mikar@gmx.de>
2014-04-02 12:31:06 +0200

    collision now

--- games/frogger/frogger.py
+++ games/frogger/frogger.py
@@ -27,8 +27,12 @@ class Obstacle(object):
         else:
             self.x += 2
         # Reset the object if it moves out of screen.
-        if abs(self.x - self.startpos[0]) > 480:
-            self.x, self.y = self.startpos
+#         if abs(self.x - self.startpos[0]) > 480:
+#             self.x, self.y = self.startpos
+        if self.x > 480:
+            self.x = 0
+        elif self.x < 0:
+            self.x = 480
         # And finally draw it.
         window.blit(self.img, (self.x, self.y))
 
@@ -64,11 +68,11 @@ class Frog(object):
 
     def left(self):
         self.status = self.img_l
-        self.x -= 40
+        self.x -= 20
 
     def right(self):
         self.status = self.img_r
-        self.x += 40
+        self.x += 20
 
     def forward(self):
         self.status = self.img_f
@@ -138,20 +142,39 @@ def main():
     c1b = Car(280, 520, "data/car_1.png", 1)
     c1c = Car(120, 520, "data/car_1.png", 1)
     c2a = Car(0, 480, "data/car_2.png")
+    c2b = Car(160, 480, "data/car_2.png")
+    c2c = Car(320, 480, "data/car_2.png")
     c3a = Car(440, 440, "data/car_3.png", 1)
+    c3b = Car(280, 440, "data/car_3.png", 1)
+    c3c = Car(120, 440, "data/car_3.png", 1)
     c4a = Car(0, 400, "data/car_4.png")
+    c4b = Car(160, 400, "data/car_4.png")
+    c4c = Car(320, 400, "data/car_4.png")
     c5a = Car(440, 370, "data/car_5.png", 1)
+    c5b = Car(280, 370, "data/car_5.png", 1)
+    c5c = Car(120, 370, "data/car_5.png", 1)
     t1a = Turtle(440, 280, "data/turtle_3_full.png", 1)
+    t1b = Turtle(280, 280, "data/turtle_3_full.png", 1)
+    t1c = Turtle(120, 280, "data/turtle_3_full.png", 1)
     L1a = Log(0, 240, "data/tree_small.png")
+    L1b = Log(160, 240, "data/tree_small.png")
+    L1c = Log(320, 240, "data/tree_small.png")
     L2a = Log(0, 200, "data/tree_big.png")
+    L2b = Log(200, 200, "data/tree_big.png")
     t2a = Turtle(440, 160, "data/turtle_2_full.png", 1)
+    t2b = Turtle(280, 160, "data/turtle_2_full.png", 1)
+    t2c = Turtle(120, 160, "data/turtle_2_full.png", 1)
     L3a = Log(0, 120, "data/tree_medium.png")
+    L3b = Log(160, 120, "data/tree_medium.png")
+    L3c = Log(320, 120, "data/tree_medium.png")
 
     while True:
         # Draw the images.
         window.blit(background, (0, 0))
-        objects = [f, c1a, c1b, c1c, c2a, c3a, c4a, c5a, t1a, t2a,
-                   L1a, L2a, L3a]
+        objects = [f, c1a, c1b, c1c, c2a, c2b, c2c, c3a, c3b, c3c,
+                   c4a, c4b, c4c, c5a, c5b, c5c,
+                   t1a, t1b, t1c, t2a, t2b, t2c,
+                   L1a, L1b, L1c, L2a, L2b, L3a, L3b, L3c]
         for i in objects:
             i.draw()
 #             i.rect.clamp_ip(game_zone)
