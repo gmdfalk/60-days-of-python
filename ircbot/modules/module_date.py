335e2cca4347805c4d92e87e5c74321b396746a7
max demian <mikar@gmx.de>
2014-04-08 15:11:21 +0200

    finished date module

--- ircbot/modules/module_date.py
+++ ircbot/modules/module_date.py
@@ -1,13 +1,19 @@
 import datetime
+from dateutil.tz import tzlocal
 
 
 def command_date(bot, user, channel, args):
+    "Shows date information. Usage: date [now|epoch]"
     days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
             "Saturday", "Sunday"]
 
-    today = datetime.datetime.today().weekday()
+    d = datetime.datetime.now(tzlocal())
+    tomorrow = days[d.weekday() + 1]
+    n = d.strftime("%Z/UTC%z: %H:%M:%S, %Y-%m-%d (%A, %j/%U)")
 
-    if args == "today":
-        bot.say(channel, "Today is {}".format(days[today]))
+    if args == "now":
+        bot.say(channel, n)
+    elif args == "epoch":
+        bot.say(channel, "{}".format(d.strftime("%ss since 01.01.1970")))
     else:
-        bot.say(channel, "Tomorrow is {}.".format(days[today + 1]))
+        bot.say(channel, "Tomorrow is {}.".format(tomorrow))
