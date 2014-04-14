4dfdcd338208925c52453703c7ed7083ce35d2ef
max demian <mikar@gmx.de>
2014-04-14 11:32:24 +0200

    improvements inc

--- portscanner/scanner.py
+++ portscanner/scanner.py
@@ -2,15 +2,13 @@ import socket
 
 from multiprocessing import Pool
 
-def dns_to_ip(dns):
-    if dns.replace(".", "").isdigit():
-        return dns
-    print "Resolving", dns
-    return socket.gethostbyname(dns)
+def host_to_ip(host):
+    print "[+] Resolving", host
+    return socket.gethostbyname(host)
 
 
-def scan(target_port):
-    target_ip, port = target_port
+def scan(target):
+    target_ip, port = target
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(2)
@@ -24,17 +22,18 @@ def scan(target_port):
         return port, False
 
 if __name__ == '__main__':
-    target = raw_input('Target IP: ')
-    target_ip = dns_to_ip(target)
-    print "Scanning", target_ip
+    target = raw_input('Target Host/IP: ')
+    if not target.replace(".", "").isdigit():
+        target = host_to_ip(target)
+    print "[+] Scanning", target
 
     ports = xrange(1, 1025)
-    scanlist = [(target_ip, port) for port in ports]
+    scanlist = [(target, port) for port in ports]
 
     # Use 512 workers. Not sure how insane that is but it seems to work fine.
     pool = Pool(512)
 
     for port, status in pool.imap_unordered(scan, scanlist):
         if status:
-            print port, "is open"
-    print "Finished scanning", target_ip
+            print "[!]", port, "is open"
+    print "[+] Finished scanning", target
