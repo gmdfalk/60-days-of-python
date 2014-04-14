3904d13fcc67de3bdd90797ca6a13f5082612522
max demian <mikar@gmx.de>
2014-04-14 11:58:13 +0200

    moving on to mailchecker

--- portscanner/scanner.py
+++ portscanner/scanner.py
@@ -1,9 +1,14 @@
-import socket
+from __future__ import print_function
 
+# Threading seems to work better on Windows while multiprocessing is faster on
+# Unix, because subprocesses are a lot less expensive here.
 from multiprocessing import Pool
+import socket
+import sys
+
 
 def host_to_ip(host):
-    print "[+] Resolving", host
+    print("[+] Resolving", host)
     return socket.gethostbyname(host)
 
 
@@ -22,12 +27,21 @@ def scan(target):
         return port, False
 
 if __name__ == '__main__':
-    target = raw_input('Target Host/IP: ')
+    if not len(sys.argv):
+        print("Usage: scanner.py <target> <maxport>")
+    if len(sys.argv) == 3:
+        maxport = int(sys.argv[2])
+    else:
+        maxport = 1025
+
+    target = sys.argv[1]
+
+    # Resolve Host to IP, if necessary.
     if not target.replace(".", "").isdigit():
         target = host_to_ip(target)
-    print "[+] Scanning", target
+    print("[+] Scanning", target)
 
-    ports = xrange(1, 1025)
+    ports = range(1, maxport + 1)
     scanlist = [(target, port) for port in ports]
 
     # Use 512 workers. Not sure how insane that is but it seems to work fine.
@@ -35,5 +49,5 @@ if __name__ == '__main__':
 
     for port, status in pool.imap_unordered(scan, scanlist):
         if status:
-            print "[!]", port, "is open"
-    print "[+] Finished scanning", target
+            print("[!]", port, "is open")
+    print("[+] Finished scanning", target)
