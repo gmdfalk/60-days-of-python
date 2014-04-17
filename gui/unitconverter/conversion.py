8cfec86ada776144d4d5e022b84a8eccf1d08e1a
max demian <mikar@gmx.de>
2014-04-17 18:08:49 +0200

    length conversion pretty much finished

--- gui/unitconverter/conversion.py
+++ gui/unitconverter/conversion.py
@@ -4,23 +4,76 @@ from __future__ import division
 class Length(object):
 
     def __init__(self):
-        self.millimeters = 0
-        self.centimeters = 0
-        self.meters = 0
-        self.kilometers = 0
-        self.inches = 0
-        self.feet = 0
-        self.yards = 0
-        self.miles = 0
+        self.precision = 4  # Decimal points of accuracy.
+        self._meters = 0
 
-    def from_centimeters(self, cm):
-        self.millimeters = round(cm * 10, 3)
-        self.centimeters = round(cm, 3)
-        self.meters = round(cm / 100, 3)
-        self.kilometers = round(cm / 1000, 3)
+    @property
+    def millimeters(self):
+        return round(self._meters * 1000, self.precision)
+
+    @millimeters.setter
+    def millimeters(self, value):
+        self._meters = value / 1000
+
+    @property
+    def centimeters(self):
+        return round(self._meters * 100, self.precision)
+
+    @centimeters.setter
+    def centimeters(self, value):
+        self._meters = value / 100
+
+    @property
+    def meters(self):
+        return round(self._meters, self.precision)
+
+    @meters.setter
+    def meters(self, value):
+        self._meters = value
+
+    @property
+    def kilometers(self):
+        return round(self._meters / 1000, self.precision)
+
+    @kilometers.setter
+    def kilometers(self, value):
+        self._meters = value * 1000
+
+    @property
+    def inches(self):
+        return round(self._meters * 39.3701, self.precision)
+
+    @inches.setter
+    def inches(self, value):
+        self._meters = value / 39.3701
+
+    @property
+    def feet(self):
+        return round(self._meters * 3.28084, self.precision)
+
+    @feet.setter
+    def feet(self, value):
+        self._meters = value / 3.28084
+
+    @property
+    def yards(self):
+        return round(self._meters * 1.09361, self.precision)
+
+    @yards.setter
+    def yards(self, value):
+        self._meters = value / 1.09361
+
+    @property
+    def miles(self):
+        return round(self._meters * 0.000621371, self.precision)
+
+    @miles.setter
+    def miles(self, value):
+        self._meters = value / 0.000621371
 
 
 if __name__ == "__main__":
     L = Length()
-    L.from_centimeters(1251.12321)
-    print L.meters, L.millimeters
+    L.centimeters = 100
+    print L.millimeters, L.centimeters, L._meters, L.kilometers
+    print L.inches, L.feet, L.yards, L.miles
--- gui/unitconverter/tests/conversion_test.py
+++ gui/unitconverter/tests/conversion_test.py
@@ -14,14 +14,25 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.L = Length()
+        self.L.inches = 2
 
     def tearDown(self):
-        pass
+        self.L.precision = 4
 
-    def test_from_centimeters(self):
-        self.L.from_centimeters(123456)
-        self.assertEqual(self.L.meters, 1234.56)
+    def test_base_case(self):
+        self.assertEqual(self.L.millimeters, 50.8)
+        self.assertEqual(self.L.centimeters, 5.08)
+        self.assertEqual(self.L.meters, 0.0508)
+        self.assertEqual(self.L.kilometers, 0.0001)
 
+    def test_high_precision(self):
+        self.L.precision = 9
+        self.L.centimeters = 34.56789123456
+        self.assertEqual(self.L.millimeters, 345.678912346)
+        self.assertEqual(self.L.centimeters, 34.567891235)
+        self.assertEqual(self.L.meters, 0.345678912)
+        self.assertEqual(self.L.kilometers, 0.000345679)
+        self.assertEqual(self.L.miles, 0.000214795)
 
 if __name__ == "__main__":
     unittest.main()
