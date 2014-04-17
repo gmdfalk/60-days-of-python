f1b564dd82821a719d61b8803ec9e55b1dd28ce5
max demian <mikar@gmx.de>
2014-04-17 21:27:07 +0200

    datainput

--- gui/unitconverter/unitconverter.py
+++ gui/unitconverter/unitconverter.py
@@ -7,7 +7,7 @@ from PyQt4 import QtGui, QtCore
 from conversion import Data, Length, Volume
 
 
-class Converter(QtGui.QMainWindow):
+class Converter(QtGui.QWidget):
 
     def __init__(self):
         super(Converter, self).__init__()
@@ -18,24 +18,135 @@ class Converter(QtGui.QMainWindow):
 
         QtGui.QToolTip.setFont(QtGui.QFont("SansSerif", 10))
 
-        dbtn = QtGui.QPushButton("Quit", self)
-        dbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        dbtn.resize(dbtn.sizeHint())
-#         dbtn.move(0, 0)
-        lbtn = QtGui.QPushButton("Quit", self)
-        lbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        lbtn.resize(lbtn.sizeHint())
-        lbtn.move(100, 0)
-        vbtn = QtGui.QPushButton("Quit", self)
-        vbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        vbtn.resize(vbtn.sizeHint())
-        vbtn.move(200, 0)
+        self.create_buttons()
 
-        self.setGeometry(300, 300, 285, 150)
+        self.data = Data()
+        self.create_data_input()
+        self.dl = [self.bitlabel, self.byteslabel, self.KBfield, self.KBlabel,
+                    self.bitfield, self.bytesfield, self.MBfield, self.MBlabel,
+                    self.GBfield, self.GBlabel, self.TBfield, self.TBlabel,
+                    self.PBfield, self.PBlabel, self.KiBfield, self.KiBlabel,
+                    self.MiBfield, self.MiBlabel, self.GiBfield, self.GiBlabel,
+                    self.TiBfield, self.TiBlabel, self.PiBfield, self.PiBlabel]
+
+        # Main Window
+        self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle("UnitConverter")
         self.setWindowIcon(QtGui.QIcon("data/calculator.png"))
 
         self.show()
+
+    def create_buttons(self):
+        dbtn = QtGui.QPushButton("Data", self)
+        dbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        dbtn.resize(dbtn.sizeHint())
+
+        lbtn = QtGui.QPushButton("Length", self)
+        lbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        lbtn.resize(lbtn.sizeHint())
+        lbtn.move(100, 0)
+
+        vbtn = QtGui.QPushButton("Volume", self)
+        vbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        vbtn.resize(vbtn.sizeHint())
+        vbtn.move(200, 0)
+
+    def clear_data_input(self):
+        for i in self.dl:
+            i.deleteLater()
+
+    def create_data_input(self):
+        # Input field
+        self.bitlabel = QtGui.QLabel(self)
+        self.bitfield = QtGui.QLineEdit(self)
+        self.bitfield.move(0, 60)
+        self.bitlabel.move(130, 60)
+        self.bitlabel.setText("Bits")
+        self.bitfield.textChanged[str].connect(self.onChanged)
+
+        self.byteslabel = QtGui.QLabel(self)
+        self.bytesfield = QtGui.QLineEdit(self)
+        self.bytesfield.move(180, 60)
+        self.byteslabel.move(310, 60)
+        self.byteslabel.setText("Bytes")
+        self.bytesfield.textChanged[str].connect(self.onChanged)
+
+        self.KBlabel = QtGui.QLabel(self)
+        self.KBfield = QtGui.QLineEdit(self)
+        self.KBfield.move(0, 90)
+        self.KBlabel.move(130, 90)
+        self.KBlabel.setText("KB")
+        self.KBfield.textChanged[str].connect(self.onChanged)
+
+        self.MBlabel = QtGui.QLabel(self)
+        self.MBfield = QtGui.QLineEdit(self)
+        self.MBfield.move(0, 120)
+        self.MBlabel.move(130, 120)
+        self.MBlabel.setText("MB")
+        self.MBfield.textChanged[str].connect(self.onChanged)
+
+        self.GBlabel = QtGui.QLabel(self)
+        self.GBfield = QtGui.QLineEdit(self)
+        self.GBfield.move(0, 150)
+        self.GBlabel.move(130, 150)
+        self.GBlabel.setText("GB")
+        self.GBfield.textChanged[str].connect(self.onChanged)
+
+        self.TBlabel = QtGui.QLabel(self)
+        self.TBfield = QtGui.QLineEdit(self)
+        self.TBfield.move(0, 180)
+        self.TBlabel.move(130, 180)
+        self.TBlabel.setText("TB")
+        self.TBfield.textChanged[str].connect(self.onChanged)
+
+        self.PBlabel = QtGui.QLabel(self)
+        self.PBfield = QtGui.QLineEdit(self)
+        self.PBfield.move(0, 210)
+        self.PBlabel.move(130, 210)
+        self.PBlabel.setText("PB")
+        self.PBfield.textChanged[str].connect(self.onChanged)
+
+        self.KiBlabel = QtGui.QLabel(self)
+        self.KiBfield = QtGui.QLineEdit(self)
+        self.KiBfield.move(180, 90)
+        self.KiBlabel.move(310, 90)
+        self.KiBlabel.setText("KiB")
+        self.KiBfield.textChanged[str].connect(self.onChanged)
+
+        self.MiBlabel = QtGui.QLabel(self)
+        self.MiBfield = QtGui.QLineEdit(self)
+        self.MiBfield.move(180, 120)
+        self.MiBlabel.move(310, 120)
+        self.MiBlabel.setText("MiB")
+        self.MiBfield.textChanged[str].connect(self.onChanged)
+
+        self.GiBlabel = QtGui.QLabel(self)
+        self.GiBfield = QtGui.QLineEdit(self)
+        self.GiBfield.move(180, 150)
+        self.GiBlabel.move(310, 150)
+        self.GiBlabel.setText("GiB")
+        self.GiBfield.textChanged[str].connect(self.onChanged)
+
+        self.TiBlabel = QtGui.QLabel(self)
+        self.TiBfield = QtGui.QLineEdit(self)
+        self.TiBfield.move(180, 180)
+        self.TiBlabel.move(310, 180)
+        self.TiBlabel.setText("TiB")
+        self.TiBfield.textChanged[str].connect(self.onChanged)
+
+        self.PiBlabel = QtGui.QLabel(self)
+        self.PiBfield = QtGui.QLineEdit(self)
+        self.PiBfield.move(180, 210)
+        self.PiBlabel.move(310, 210)
+        self.PiBlabel.setText("PiB")
+        self.PiBfield.textChanged[str].connect(self.onChanged)
+
+
+    def onChanged(self, text):
+
+        pass
+#         self.lbl.setText(text)
+#         self.lbl.adjustSize()
 
     def center(self):
         qr = self.frameGeometry()
