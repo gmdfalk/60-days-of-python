b6aafed9d0301517002f4a2736a3071219e86410
max demian <mikar@gmx.de>
2014-04-17 15:56:24 +0200

    this is a button. there are many like it but this one is mine.

--- gui/unitconverter/unitconverter.py
+++ gui/unitconverter/unitconverter.py
@@ -5,14 +5,35 @@ import sys
 from PyQt4 import QtGui
 
 
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        QtGui.QToolTip.setFont(QtGui.QFont("SansSerif", 10))
+
+        self.setToolTip("This is a <b>QWidget</b> widget")
+
+        btn = QtGui.QPushButton("Button", self)
+        btn.setToolTip("This is a <b>QPushButton</b> widget")
+        btn.resize(btn.sizeHint())
+#         btn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("UnitConverter")
+        self.setWindowIcon(QtGui.QIcon("data/calculator.png"))
+
+        self.show()
+
+
 def main():
     app = QtGui.QApplication(sys.argv)
 
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle("UnitConverter")
-    w.show()
+    ex = Example()
 
     sys.exit(app.exec_())
 
