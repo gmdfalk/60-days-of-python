b75c75b05ec374d686fc8c3580aaaa21cf48fc52
max demian <mikar@gmx.de>
2014-04-17 15:38:06 +0200

    almost done

--- gui/unitconverter/unitconverter.py
+++ gui/unitconverter/unitconverter.py
@@ -1,6 +1,21 @@
-"""
-    Created on 16 Apr 2014
+#!/usr/bin/env python2
 
-    @author: Max Demian
-"""
+import sys
 
+from PyQt4 import QtGui
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle("UnitConverter")
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
