8ec7c7746f8957d4585af77b88a3f85725b2d2a5
max demian <mikar@gmx.de>
2014-04-24 16:50:57 +0200

    finally going to work on the calculator.

--- gui/calculator/calculation.py
+++ gui/calculator/calculation.py
@@ -1,15 +1,33 @@
+from string import digits, letters
 import ast
-from string import letters
+import token
 
+
+class AddToken(object):
+
+    priority = 10
+
+    def led(self, left):
+        right = Calculation().parse(10)
+        return left + right
 
 class Calculation(object):
 
     def evaluate(self, s):
         invalids = letters + "!#$&\',:;<=>?@[\\]_`{|}~"
-        if any(char in invalids for char in s):
+        if any(c in invalids for c in s) or not any(c in digits for c in s):
             return
         return eval(s)
 
+    def parse(self, s):
+        return s
+
+    def dostuff(self):
+        return AddToken().led(5)
+
+
 if __name__ == "__main__":
     c = Calculation()
-    print c.evaluate("1+2*3/4-5")
+    print c.evaluate("-+10")
+    print c.dostuff()
+
