ea6d25f9276afc4181a076b948fe16f32b4447cb
max demian <mikar@gmx.de>
2014-05-11 00:39:30 +0200

    implemented filesystemmodel view. populating options.

--- gui/filebrowser/filebrowser.py
+++ gui/filebrowser/filebrowser.py
@@ -1,10 +1,3 @@
-"""
-    Created on 1 May 2014
-
-    @author: Max Demian
-"""
-
-
 import re
 import sys
 
@@ -18,60 +11,19 @@ class FileBrowser(QtGui.QMainWindow):
         super(FileBrowser, self).__init__(parent)
         uic.loadUi("filebrowser.ui", self)
 
-        # mainwidget
-        #   mainsplitter
-        #     leftsplitter
-        #        dirtree
-        #        actiontabs
-        #            replacer_tab
-        #                nocaseradio
-        #                lowerradio
-        #                upperradio
-        #            delete_tab
-        #            append_tab
-        #            option_tab
-        #                extensioncheck
-        #                hiddencheck
-        #                previewcheck
-        #                recursecheck
-        #                statuscheck
-        #                check
-        #                extensioncheck
-        #            status_tab
-        #     rightlayout
-        #        editlayout
-        #            regexcheck
-        #            sourceedit
-        #            targetedit
-        #        browserlayout
-        #            browsertabs
-        #                both_tab
-        #                dirs_tab
-        #                files_tab
-        #                both_tab
-        #                both_tab
-        #        buttonlayout
-        #            previewbutton
-        #            redobutton
-        #            refreshbutton
-        #            undobutton
-        # Main Window
-#         self.setGeometry(300, 200, 760, 600)
-#         self.setWindowTitle("FileBrowser")
-#         self.setWindowIcon(QtGui.QIcon("data/icon.png"))
-#
-#         QtGui.QToolTip.setFont(QtGui.QFont("SansSerif", 10))
-#
-#         model = QtGui.QFileSystemModel()
-#         model.setRootPath("")
-#         model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot |
-#                         QtCore.QDir.Hidden)
-#         view = QtGui.QTreeView()
-#         view.setModel(model)
-#         view.setColumnHidden(1, True)
-#         view.setColumnHidden(2, True)
-#         view.setColumnHidden(3, True)
-#         view.header().hide()
+        self.setWindowTitle("FileBrowser")
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+
+        model = QtGui.QFileSystemModel()
+        model.setRootPath("")
+        model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot |
+                        QtCore.QDir.Hidden)
+
+        self.dirtree.setModel(model)
+        self.dirtree.setColumnHidden(1, True)
+        self.dirtree.setColumnHidden(2, True)
+        self.dirtree.setColumnHidden(3, True)
+        self.dirtree.header().hide()
 #         self.setCentralWidget(view)
 
 
