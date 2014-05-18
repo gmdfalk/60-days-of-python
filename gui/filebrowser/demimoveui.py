538cea6363a112ce80bdf9ed67ba2db6afaa8ccc
max demian <mikar@gmx.de>
2014-05-18 20:52:32 +0200

    banking history for now

--- gui/filebrowser/demimoveui.py
+++ gui/filebrowser/demimoveui.py
@@ -19,12 +19,13 @@ Options:
 #        Fixed temporariliy by commenting filter changes.
 # FIXME: Fix performance on many files (recursive)?
 import logging
+import os
 import sys
 
 from PyQt4 import QtGui, QtCore, uic
 
 from fileops import FileOps
-import os
+import history
 
 
 log = logging.getLogger("gui")
@@ -34,150 +35,6 @@ try:
 except ImportError:
     print "ImportError: Please install docopt to use the CLI."
 
-
-class TreeItem(object):
-    def __init__(self, data, parent=None):
-        self.parentItem = parent
-        self.itemData = data
-        self.childItems = []
-
-    def appendChild(self, item):
-        self.childItems.append(item)
-
-    def child(self, row):
-        return self.childItems[row]
-
-    def childCount(self):
-        return len(self.childItems)
-
-    def columnCount(self):
-        return len(self.itemData)
-
-    def data(self, column):
-        try:
-            return self.itemData[column]
-        except IndexError:
-            return None
-
-    def parent(self):
-        return self.parentItem
-
-    def row(self):
-        if self.parentItem:
-            return self.parentItem.childItems.index(self)
-        return 0
-
-
-class HistoryTreeModel(QtCore.QAbstractItemModel):
-
-    def __init__(self, parent=None):
-        super(HistoryTreeModel, self).__init__(parent)
-        self.p = parent
-
-        self.rootItem = TreeItem(("Title", "Summary"))
-        self.setupModelData(parent.previews, self.rootItem)
-
-    def columnCount(self, parent):
-        if parent.isValid():
-            return parent.internalPointer().columnCount()
-        else:
-            return self.rootItem.columnCount()
-
-    def data(self, index, role):
-        if not index.isValid():
-            return None
-
-        if role != QtCore.Qt.DisplayRole:
-            return None
-
-        item = index.internalPointer()
-
-        return item.data(index.column())
-
-    def flags(self, index):
-        if not index.isValid():
-            return QtCore.Qt.NoItemFlags
-
-        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
-
-    def headerData(self, section, orientation, role):
-        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
-            return self.rootItem.data(section)
-
-        return None
-
-    def index(self, row, column, parent):
-        if not self.hasIndex(row, column, parent):
-            return QtCore.QModelIndex()
-
-        if not parent.isValid():
-            parentItem = self.rootItem
-        else:
-            parentItem = parent.internalPointer()
-
-        childItem = parentItem.child(row)
-        if childItem:
-            return self.createIndex(row, column, childItem)
-        else:
-            return QtCore.QModelIndex()
-
-    def parent(self, index):
-        if not index.isValid():
-            return QtCore.QModelIndex()
-
-        childItem = index.internalPointer()
-        parentItem = childItem.parent()
-
-        if parentItem == self.rootItem:
-            return QtCore.QModelIndex()
-
-        return self.createIndex(parentItem.row(), 0, parentItem)
-
-    def rowCount(self, parent):
-        if parent.column() > 0:
-            return 0
-
-        if not parent.isValid():
-            parentItem = self.rootItem
-        else:
-            parentItem = parent.internalPointer()
-
-        return parentItem.childCount()
-
-    def setupModelData(self, lines, parent):
-        parents = [parent]
-        indentations = [0]
-
-        number = 0
-
-        while number < len(lines):
-            position = 0
-            while position < len(lines[number]):
-                if lines[number][position] != ' ':
-                    break
-                position += 1
-
-            lineData = lines[number][position:]
-
-            number += 1
-            if not lineData:
-                continue
-
-            # Read the column data from the rest of the line.
-#             columnData = [s for s in lineData.split('\t') if s]
-
-            if position > indentations[-1]:
-                # The last child of the current parent is now the new
-                # parent unless the current parent has no children.
-                if parents[-1].childCount() > 0:
-                    parents.append(parents[-1].child(parents[-1].childCount() - 1))
-                    indentations.append(position)
-            else:
-                while position < indentations[-1] and len(parents) > 0:
-                    parents.pop()
-                    indentations.pop()
-            # Append a new item to the current parent's list of children.
-            parents[-1].appendChild(TreeItem(lines, parents[-1]))
 
 
 class BoldDelegate(QtGui.QStyledItemDelegate):
@@ -289,7 +146,7 @@ class DemiMoveGUI(QtGui.QMainWindow):
         self.dirtree.setCurrentIndex(index)
 
     def create_historytab(self):
-        self.historymodel = HistoryTreeModel(self)
+        self.historymodel = history.HistoryTreeModel(parent=self)
         self.historytree.setModel(self.historymodel)
 
     def set_cwd(self):
@@ -325,7 +182,6 @@ class DemiMoveGUI(QtGui.QMainWindow):
         if self.cwd:
             self.previews = self.fileops.get_preview(self.targets, self.matchpat,
                                                      self.replacepat)
-            self.historymodel.setupModelData(self.previews, self)
         else:
             self.previews = []
         self.update_view()
